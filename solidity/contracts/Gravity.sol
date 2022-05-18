b23f320b2523f254e616546dc071646fb07b59cd
Valentin Staykov <v.staykov@razorlabs.com>
2022-05-18 09:49:54 +0000

    CUDOS-974 gravityId immutable

--- solidity/contracts/Gravity.sol
+++ solidity/contracts/Gravity.sol
@@ -56,7 +56,7 @@ contract Gravity is ReentrancyGuard {
 	uint256 public state_lastEventNonce = 1;
 
 	// These are set once at initialization
-	bytes32 public state_gravityId;
+	bytes32 public immutable state_gravityId;
 	uint256 public immutable state_powerThreshold;
 
 	CudosAccessControls public immutable cudosAccessControls;
