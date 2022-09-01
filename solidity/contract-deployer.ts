98971ae2eff2f7ba96e8bfa5123e03fe33cf1cb7
Valentin Staykov <v.staykov@gmail.com>
2022-09-01 12:21:21 +0000

    added gravity contract verify

--- solidity/contract-deployer.ts
+++ solidity/contract-deployer.ts
@@ -10,6 +10,7 @@ import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from "axios";
 import { exit } from "process";
 import { start } from "node:repl";
 import { SSL_OP_EPHEMERAL_RSA } from "node:constants";
+import  hre from "hardhat";
 
 const args = commandLineArgs([
   // the ethernum node used to deploy the contract
@@ -229,6 +230,19 @@ async function deploy() {
   await gravity.deployed();
   console.log("Gravity deployed at Address - ", gravity.address);
   await submitGravityAddress(gravity.address);
+
+  await gravity.deployTransaction.wait(10)
+  console.log("Verifying contract on Etherscan...");
+
+  await hre.run("verify:verify", {
+    address: gravity.address,
+    constructorArguments: [
+      gravityId,
+      vote_power,
+      eth_addresses,
+      powers,
+    ],
+  });
 }
 
 function getContractArtifacts(path: string): { bytecode: string; abi: string } {
