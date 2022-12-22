19c9dc20c032a873b4d9d04bbf742dc50f27a76e
ealeksandrov-LimeChain <92088769+ealeksandrov-LimeChain@users.noreply.github.com>
2022-12-22 17:14:52 +0200

    Update contract-deployer.sh

--- solidity/scripts/contract-deployer.sh
+++ solidity/scripts/contract-deployer.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 npx ts-node \
 contract-deployer.ts \
---cosmos-node="http://localhost:26657" \
---eth-node="http://localhost:8545" \
---eth-privkey="0xb1bab011e03a9862664706fc3bbaa1b16651528e5f0e7fbfcbfdd8be302a13e7" \
+--cosmos-node="${COSMOS_NODE}" \
+--eth-node="${ETH_NODE}" \
+--eth-privkey="${ETH_PRIV_KEY_HEX}" \
 --contract="artifacts/contracts/Gravity.sol/Gravity.json" \
 --test-mode=true \
---cudos-access-control="0x0412C7c846bb6b7DC462CF6B453f76D8440b2609" \
---cudos-token-address="0x9676519d99E390A180Ab1445d5d857E3f6869065"
+--cudos-access-control="${CUDOS_ACCESS_CONTROL_ADDRESS}" \
+--cudos-token-address="${CUDOS_TOKEN_ADDRESS}"
